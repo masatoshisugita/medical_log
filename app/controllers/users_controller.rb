61de01453ac42ea6e0e3f0385eeb4c11ad32605a
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-03-31 11:22:09 +0900

    ログイン機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit,:update,:destroy]
+  #skip_before_action :login_required, only: [:index, :new]
+
   def index
   end
 
@@ -9,28 +12,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: "「#{@user.name}」を登録しました。"
+      redirect_to login_url, notice: "「#{@user.name}」を登録しました。"
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update!(user_params)
     redirect_to @user, notice:"タスク「#{@user.name}」を更新しました。"
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to root_url, notice:"タスク「#{@user.name}」を削除しました。"
   end
@@ -38,6 +37,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,:email,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users
+
+  get '/login',to: 'sessions#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout',to: 'sessions#destroy'
 end
