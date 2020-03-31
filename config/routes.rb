fce006646c57e69d16913e9725a445b700d0cad4
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-03-31 16:24:32 +0900

    Topicの基本的なCRUD機能の完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users
+  resources :topics
 
   get '/login',to: 'sessions#new'
   post '/login',to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_044134) do
+ActiveRecord::Schema.define(version: 2020_03_31_024103) do
+
+  create_table "topics", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "sick_name"
+    t.string "period"
+    t.string "initial_symptom"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
