226cd0d159a5db0a3ae06f5dad2523ad1dac9212
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-04-03 15:38:13 +0900

    Relationshipモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 
   has_many :topics, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: follower_id, dependent: :destroy #フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: followed_id, dependent: :destroy　#フォロワー取得
+  has_many :following_user, through: :follower,  source: :followed #自分がフォローしている人
+  has_many :follower_user, through: :followed,  source: :follower #自分をフォローしている人
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_061126) do
+ActiveRecord::Schema.define(version: 2020_04_03_061820) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "topic_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id"
   end
 
   create_table "topics", force: :cascade do |t|
