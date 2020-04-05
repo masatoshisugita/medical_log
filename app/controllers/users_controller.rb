9e1be750659879ed822bde1cf1cb508b36f59abc
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-04-05 15:55:39 +0900

    フォロー・フォロワー一覧機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,18 +31,16 @@ class UsersController < ApplicationController
     redirect_to root_url, notice:"タスク「#{@user.name}」を削除しました。"
   end
 
-  def following_user
-    #@userがフォローしているユーザー
+  #@userがフォローしているユーザーを表示するメソッド
+  def following
     @user = User.find(params[:id])
     @users = @user.following_user
-    render 'show_follow'
   end
 
-  def follower_user
-    #@userをフォローしているユーザ
+  #@userをフォローしているユーザーを表示するメソッド
+  def followers
     @user = User.find(params[:id])
     @users = @user.follower_user
-    render 'show_follower'
   end
 
   private
