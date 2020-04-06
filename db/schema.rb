9d8571b6f1c25055bc71af75e3355360d7dddf6a
masatoshi sugita <mr.f-r.ms621@i.softbank.jp>
2020-04-06 16:06:50 +0900

    Commentテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_061820) do
+ActiveRecord::Schema.define(version: 2020_04_06_070415) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "topic_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
